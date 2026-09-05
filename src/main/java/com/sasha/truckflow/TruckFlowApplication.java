9a65b3fd9682ac6e287ba1029de8dfce24cc4ef9
Sasha Uhriumov <ouhriumov2007@gmail.com>
2026-09-05 16:09:01 -0700

    TruckFlow

---
+++ src/main/java/com/sasha/truckflow/TruckFlowApplication.java
@@ -0,0 +1,13 @@
+package com.sasha.truckflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruckFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TruckFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sasha/truckflow/TruckFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sasha.truckflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruckFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
