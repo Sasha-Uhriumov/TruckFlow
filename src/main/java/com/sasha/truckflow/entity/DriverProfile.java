0fe9b20ee0c138602ef65eb2bb9d565093b0bdb2
Sasha Uhriumov <ouhriumov2007@gmail.com>
2026-09-07 15:22:30 -0700

    feat: add driver profile entity

---
+++ src/main/java/com/sasha/truckflow/entity/DriverProfile.java
@@ -0,0 +1,36 @@
+package com.sasha.truckflow.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "driver_profiles")
+public class DriverProfile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "first_name", length = 20, nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", length = 30, nullable = false)
+    private String lastname;
+
+    @Column(name = "phone_number", length = 15, nullable = false, unique = true)
+    private String phoneNumber;
+
+    @Column(name = "license_number", length = 10, nullable = false, unique = true)
+    private String licenseNumber;
+
+    @Column(name = "address", length = 100, nullable = false)
+    private String address;
+}
