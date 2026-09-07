160cb365a58971658f11f296ff8616650ffdffb1
Sasha Uhriumov <ouhriumov2007@gmail.com>
2026-09-07 16:47:29 -0700

    feat: add truck entity

--- src/main/java/com/sasha/truckflow/entity/Truck.java
+++ src/main/java/com/sasha/truckflow/entity/Truck.java
@@ -1,6 +1,6 @@
 package com.sasha.truckflow.entity;
 
-import com.sasha.truckflow.enums.RoleType;
+import com.sasha.truckflow.enums.TruckType;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,15 +12,21 @@ import lombok.Setter;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "roles")
-public class Roles {
+@Table(name = "trucks")
+public class Truck {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
+    @Column(name = "make", length = 30, nullable = false)
+    private String make;
+
+    @Column(name = "capacity", nullable = false)
+    private Integer capacity;
+
     @Enumerated(EnumType.STRING)
-    @Column(name = "role", length = 10, nullable = false)
-    private RoleType roleType;
+    @Column(name = "truck_type", length = 30, nullable = false)
+    private TruckType truckType;
 }
