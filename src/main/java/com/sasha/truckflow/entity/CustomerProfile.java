9d011281420b22f4f2456964c445e28ce9208b89
Sasha Uhriumov <ouhriumov2007@gmail.com>
2026-09-07 15:54:31 -0700

    feat: add customer profile entity

---
+++ src/main/java/com/sasha/truckflow/entity/CustomerProfile.java
@@ -0,0 +1,36 @@
+package com.sasha.truckflow.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "customer_profiles")
+public class CustomerProfile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "first_name", length = 20, nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", length = 30, nullable = false)
+    private String lastName;
+
+    @Column(name = "phone_number", length = 15, nullable = false, unique = true)
+    private String phoneNumber;
+
+    @Column(name = "company_name", length = 10, nullable = false)
+    private String companyName;
+
+    @Column(name = "address", length = 100, nullable = false)
+    private String address;
+}
