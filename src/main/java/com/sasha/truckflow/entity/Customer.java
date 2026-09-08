8c0e53f9763a3f9affa40b9493b88fe45f93257e
Sasha Uhriumov <ouhriumov2007@gmail.com>
2026-09-07 22:01:10 -0700

    feat add consumer entity

---
+++ src/main/java/com/sasha/truckflow/entity/Customer.java
@@ -0,0 +1,30 @@
+package com.sasha.truckflow.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "customers")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @OneToOne
+    @JoinColumn(name = "customer_profile_id")
+    private CustomerProfile customerProfile;
+
+    //TODO upload orders
+}
