76666ee3b0051a01b882b96759c0d317bedcc011
Sasha Uhriumov <ouhriumov2007@gmail.com>
2026-09-07 17:16:16 -0700

    feat: add table users to database

--- src/main/java/com/sasha/truckflow/entity/User.java
+++ src/main/java/com/sasha/truckflow/entity/User.java
@@ -5,10 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 
 @Entity
 @Setter
