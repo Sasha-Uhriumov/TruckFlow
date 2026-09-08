6c5dd2e3bd0dbe37ea6be70058542e55aaa5864a
Sasha Uhriumov <ouhriumov2007@gmail.com>
2026-09-07 22:45:46 -0700

    add: driver entity

---
+++ src/main/java/com/sasha/truckflow/entity/Driver.java
@@ -0,0 +1,34 @@
+package com.sasha.truckflow.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "drivers")
+public class Driver {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @OneToOne
+    @JoinColumn(name = "driver_profile_id")
+    private DriverProfile driverProfile;
+
+    @OneToOne
+    @JoinColumn(name = "truck_id", nullable = false)
+    private Truck truck;
+}
+
